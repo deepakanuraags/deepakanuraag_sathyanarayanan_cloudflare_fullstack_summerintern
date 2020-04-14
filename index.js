4f32f49eb885b9b9da105ee7d2f5bd4f27e6cd8d
deepakanuraag sathyanarayanan <deepakanuraag@deepakaraagsair.home>
2020-04-14 04:18:19 -0400

    implemented request distribution between two variants

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var URLS = undefined;
+var toggleString = "server1";
 addEventListener("fetch", event => {
   event.respondWith(handleRequest(event.request));
 });
@@ -8,10 +9,20 @@ addEventListener("fetch", event => {
  */
 async function handleRequest(request) {
   return new Promise((resolve, reject) => {
-    fetch("https://cfw-takehome.developers.workers.dev/api/variants")
-      .then(data => data.json())
-      .then(data => data["variants"])
-      .then(data => resolve(fetch(data[0])));
+    if (!URLS) {
+      console.log("fetching URLS");
+      fetch("https://cfw-takehome.developers.workers.dev/api/variants")
+        .then(data => data.json())
+        .then(data => {
+          URLS = data["variants"];
+        })
+        .then(() => {
+          resolve(getWebPage());
+        });
+    } else {
+      console.log("not fetching URLS");
+      resolve(getWebPage());
+    }
   });
   // let response = await fetch(
   //   "https://cfw-takehome.developers.workers.dev/api/variants"
@@ -19,4 +30,15 @@ async function handleRequest(request) {
   // let parsedResp = await response.json();
   // console.log(JSON.stringify(parsedResp));
   // return await fetch(parsedResp["variants"][0]);
+}
+
+async function getWebPage() {
+  if (toggleString == "server1") {
+    toggleString = "server2";
+    return fetch(URLS[0]);
+  }
+  if (toggleString == "server2") {
+    toggleString = "server1";
+    return fetch(URLS[1]);
+  }
 }
